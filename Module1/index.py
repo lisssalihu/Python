583f809bf3aab529b4d7065ffb6f05618c2164b9
lisssalihu <167891216+lisssalihu@users.noreply.github.com>
2025-10-23 16:18:35 +0200

    Add files via upload

---
+++ Module1/index.py
@@ -0,0 +1,28 @@
+# print ('Hello World')
+#
+# temp=98.6
+# name='Ensar'
+#
+# print(name)
+#
+# print(type(name))
+# print(type(temp))
+#
+# print('The best student in class is', name)
+#
+# x=5
+# y=2
+# result=x+y
+#
+# print(result)
+
+shopping_list=['apples','oranges',3.5,4.5]
+
+print(shopping_list[2])
+
+shopping_list.append('milk')
+# shopping_list.insert(__index:2,__object:'lemons')
+
+shopping_list.remove(3.5)
+shopping_list.pop(1)
+print(shopping_list)
